6e2d8680e75464744616f21b843e806b9fc25664
k1cker666 <pomoymu@gmail.com>
2024-06-18 21:52:16 +0300

    feat public_api: add settings file

---
+++ memes/public_backend/src/settings.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_prefix="psql_", env_file_encoding="utf-8"
+    )
+    dbname: str
+    user: str
+    password: str
+    host: str
+    port: str
+
+    @property
+    def conninfo(self):
+        return f"postgresql+psycopg://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbname}"
+
+
+settings = Settings()

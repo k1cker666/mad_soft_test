53aa2edf83a17f905198a87d97eb3678b4726dac
k1cker666 <pomoymu@gmail.com>
2024-06-18 14:54:43 +0300

    feat public_api: add router for memes

--- memes/public_backend/main.py
+++ memes/public_backend/main.py
@@ -1,33 +1,15 @@
-import random
-from typing import Annotated
-
-from fastapi import FastAPI, Path
+from fastapi import FastAPI
+from src.routers.memes import router as memes_router
 
 
 app = FastAPI()
 
-
-@app.get("/memes/")
-def get_list_memes():
-    return {"memes": "list"}
+app.include_router(memes_router)
 
 
-@app.get("/memes/{id}/")
-def get_meme_from_id(id: Annotated[int, Path(ge=1)]):
-    memes = ["funny meme", "not funny meme", "best meme", "worst meme"]
-    return {"meme": {"meme_id": id, "meme": random.choice(memes)}}
-
-
-@app.post("/memes/")
-def add_new_meme(meme, caption: str):
-    return {"add": "success"}
-
-
-@app.put("/memes/{id}")
-def update_new_meme(meme):
-    return {"update": "success"}
-
-
-@app.delete("/memes/{id}")
-def delete_new_meme(meme, caption: str):
-    return {"delete": "success"}
+@app.get("/")
+def main():
+    return {
+        "detail": "Memes home page",
+        "message": "Welcome to memes home page",
+    }
---
+++ memes/public_backend/src/routers/memes.py
@@ -0,0 +1,33 @@
+import random
+from typing import Annotated
+
+from fastapi import APIRouter, Path
+
+
+router = APIRouter(prefix="/memes")
+
+
+@router.get("/")
+def get_list_memes():
+    return {"memes": "list"}
+
+
+@router.get("/{id}/")
+def get_meme_from_id(id: Annotated[int, Path(ge=1)]):
+    memes = ["funny meme", "not funny meme", "best meme", "worst meme"]
+    return {"meme": {"meme_id": id, "meme": random.choice(memes)}}
+
+
+@router.post("/")
+def add_new_meme(meme, caption: str):
+    return {"add": "success"}
+
+
+@router.put("/{id}")
+def update_new_meme(meme):
+    return {"update": "success"}
+
+
+@router.delete("/{id}")
+def delete_new_meme(meme, caption: str):
+    return {"delete": "success"}

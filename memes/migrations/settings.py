9a29d309148205b5a2f968cf898bb89b78a7b4a9
k1cker666 <pomoymu@gmail.com>
2024-06-21 23:08:03 +0300

    feat migrations: add dockerfile

--- memes/migrations/settings.py
+++ memes/migrations/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", env_prefix="psql_", env_file_encoding="utf-8"
+        env_file=".psql_env", env_prefix="psql_", env_file_encoding="utf-8"
     )
     dbname: str
     user: str
